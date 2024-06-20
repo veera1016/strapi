83621e92495918cae44b40d4b14f27dba30b387d
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-06-20 23:46:14 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.27.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
 }
