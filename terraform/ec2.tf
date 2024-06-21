cf0bc08033bab93ae4bd671d11ca317190ad2f28
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-06-21 21:40:36 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -34,10 +34,6 @@ resource "aws_instance" "strapi" {
   }
 }
 
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-security-group"
-  description = "Security group for Strapi EC2 instance"
-
   ingress {
     from_port   = 22
     to_port     = 22
