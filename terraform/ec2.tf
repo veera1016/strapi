a6f1411e9f62318e139940691ce43858c7861555
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-06-21 23:26:07 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ variable "private_key_path" {
 }
 
 resource "aws_instance" "strapi" {
-  ami           = "ami-0f58b397bc5c1f2e8"  # Correct AMI ID for ap-south-1
+  ami           = "ami-04b70fa74e45c3917"  # Correct AMI ID for ap-south-1
   instance_type = "t2.small"               # Changed to t2.small
   key_name      = "Veera"                  # Your key pair name
 
