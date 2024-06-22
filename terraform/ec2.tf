8c8ee6619513dc7efe6eceea831510c6fdcbce81
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-06-22 12:16:19 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -46,11 +46,7 @@ resource "aws_instance" "strapi" {
       "curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -",
       "sudo apt-get install -y nodejs",
       "sudo npm install -g pm2",
-      "sudo mkdir -p /srv/strapi",
-      "sudo chown ubuntu:ubuntu /srv/strapi",
       "git clone -b revert-1-master https://github.com/veera1016/strapi.git /srv/strapi",
-      "ls -la /srv/strapi",  # List the contents of the cloned directory
-      "cd /srv/strapi && ls -la",  # Ensure we are in the correct directory
       "cd /srv/strapi && npm install",
       "cd /srv/strapi && npm run build",
       "pm2 start npm --name strapi -- start"
