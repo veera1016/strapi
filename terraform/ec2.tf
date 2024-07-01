e71d4f883ef95ce4968d45088a18460628f2c3d3
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-01 07:49:43 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "strapi" {
       "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash",
       "export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"",
+      [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion",
       "source ~/.bashrc",
       "nvm install 18 ",
       "sudo npm install -g pm2 ",
